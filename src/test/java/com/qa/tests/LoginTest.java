26843d1645aa27b979b44829f7e68e5558902ee4
Kameswaran Rengarajan <kameswaranrengarajan@Kameswarans-MacBook-Pro.local>
2021-01-08 20:50:22 +0530

    first git commit

---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+import org.testng.annotations.BeforeMethod;
+
+public class LoginTest {
+	
+	@BeforeMethod
+	public void logintest() {
+		
+		System.out.println("Test Case");
+		
+	}
+}
